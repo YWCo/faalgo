9cd41a06355e6857fda173abc19764c782b7990f
YWCo <124361361+YWCo@users.noreply.github.com>
2023-09-04 12:53:19 +0100

    Delete testfinancetoolkit.py

--- testfinancetoolkit.py
+++
@@ -1,8 +0,0 @@
-from financetoolkit import Toolkit
-import streamlit as st
-
-companies = Toolkit(['AAPL', 'MSFT'], api_key='e1f7367c932b9ff3949e05adf400970c' )
-
-# a Historical example
-historical_data = companies.get_historical_data().head()
-st.dataframe(historical_data)
