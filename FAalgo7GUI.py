2e037ee50ba6e35660a33fa3390a1d6f35f1e867
YWCo <124361361+YWCo@users.noreply.github.com>
2023-09-04 22:39:20 +0100

    Update FAalgo7GUI.py

--- FAalgo7GUI.py
+++ FAalgo7GUI.py
@@ -14,14 +14,14 @@ st.set_page_config(page_title="Fundamental Analysis Algo",
                     )
 
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+#hide_streamlit_style = """
+#            <style>
+#            #MainMenu {visibility: hidden;}
+#            footer {visibility: hidden;}
+#            header {visibility: hidden;}
+#            </style>
+#            """
+#st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 
